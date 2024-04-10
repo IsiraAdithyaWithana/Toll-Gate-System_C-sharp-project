1a9b608e9bcb7dceaafdde292583ffff3b52e39a
MandinuBala <balasooriyamandinu@gmail.com>
2024-04-10 23:40:58 +0530

    gg

--- Expressway Admin loin/Form17.cs
+++ Expressway Admin loin/Form17.cs
@@ -16,5 +16,10 @@ namespace Expressway_Admin_loin
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
