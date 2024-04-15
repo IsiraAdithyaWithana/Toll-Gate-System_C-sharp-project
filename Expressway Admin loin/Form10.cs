1cf58af0e5b03b9622b8bd435711cd9d3e10c8fe
nckodikara@students.nsbm.ac.lk <nckodikara@students.nsbm.ac.lk>
2024-04-15 13:38:00 +0530

    Created the connection

--- Expressway Admin loin/Form10.cs
+++ Expressway Admin loin/Form10.cs
@@ -7,13 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace Expressway_Admin_loin
 {
     public partial class Form10 : Form
     {
         public Form10()
         {
+            SqlConnection con1 = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\DELL\OneDrive - NSBM\Desktop\NSBM\C#lab\Highway_project\Expressway Admin loin\Database1.mdf"";Integrated Security=True");
+
             InitializeComponent();
         }
 
@@ -28,6 +30,11 @@ namespace Expressway_Admin_loin
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
