25ac3866113d3c02fd1bcc2bcee77e56798222b4
Isira Adithya Withana <issaadithyawithana@gmail.com>
2024-04-19 00:12:42 +0530

    removed

--- Expressway Admin loin/Form1.cs
+++ Expressway Admin loin/Form1.cs
@@ -24,9 +24,7 @@ namespace Expressway_Admin_loin
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form2 form2 = new Form2();
-            form2.Show();
-            this.Hide();
+            
         }
     }
 }
