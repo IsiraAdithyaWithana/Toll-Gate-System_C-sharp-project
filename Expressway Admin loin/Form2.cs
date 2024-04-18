529318415d0892229a936ee1a3ae08eead51a8c8
Isira Adithya Withana <issaadithyawithana@gmail.com>
2024-04-19 02:31:57 +0530

    form 1 and 2 Completed

--- Expressway Admin loin/Form2.cs
+++ Expressway Admin loin/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -39,9 +40,63 @@ namespace Expressway_Admin_loin
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form3 form3 = new Form3();
-            form3.Show();
-            this.Hide();
+            try
+            {
+                using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\isira\source\repos\Toll-Gate-System_C-sharp-project1\Expressway Admin loin\Database1.mdf"";Integrated Security=True"))
+                {
+                    con.Open();
+
+                    string UserName = txtUsername.Text;
+                    string Password = txtPassword.Text;
+
+                    string query = $"SELECT password, user_position FROM [User] WHERE username = '{UserName}';";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            string dbPassword = reader["password"].ToString();
+                            string userPosition = reader["user_position"].ToString();
+
+                            if (userPosition == "user")
+                            {
+                                if (dbPassword == Password)
+                                {
+                                    Form3 form3 = new Form3();
+                                    form3.Show();
+                                    this.Hide();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Ünsuccessful data entry. Please check your Password again");
+                                }
+                            }
+                            else
+                            {
+                                if (dbPassword == Password)
+                                {
+                                    Form16 form16 = new Form16();
+                                    form16.Show();
+                                    this.Hide();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Ünsuccessful data entry. Please check your Password again");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("User not found. Please check your username again.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured: " + ex.Message);
+            }
         }
 
         private void txtUsername_TextChanged(object sender, EventArgs e)
