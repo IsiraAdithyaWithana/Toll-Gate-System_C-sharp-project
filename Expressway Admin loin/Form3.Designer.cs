7c8a8f7a6547d43b168215d7a5b7f1034f954bed
Isira Adithya Withana <issaadithyawithana@gmail.com>
2024-04-19 20:56:03 +0530

    Form3 Button and others were named

--- Expressway Admin loin/Form3.Designer.cs
+++ Expressway Admin loin/Form3.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtPoliceID = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
+            this.butOK = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioEntrance = new System.Windows.Forms.RadioButton();
+            this.radioExit = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // label1
@@ -51,14 +51,14 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Southern Expressway";
             // 
-            // textBox1
+            // txtPoliceID
             // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox1.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(668, 323);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(300, 38);
-            this.textBox1.TabIndex = 7;
+            this.txtPoliceID.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtPoliceID.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPoliceID.Location = new System.Drawing.Point(668, 323);
+            this.txtPoliceID.Name = "txtPoliceID";
+            this.txtPoliceID.Size = new System.Drawing.Size(300, 38);
+            this.txtPoliceID.TabIndex = 7;
             // 
             // label5
             // 
@@ -86,19 +86,19 @@
             this.label2.TabIndex = 8;
             this.label2.Text = "Select the Gate";
             // 
-            // button4
+            // butOK
             // 
-            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button4.BackColor = System.Drawing.Color.Gray;
-            this.button4.Font = new System.Drawing.Font("Franklin Gothic Medium", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.ForeColor = System.Drawing.Color.Black;
-            this.button4.Location = new System.Drawing.Point(586, 432);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(154, 58);
-            this.button4.TabIndex = 12;
-            this.button4.Text = "OK";
-            this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.butOK.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.butOK.BackColor = System.Drawing.Color.Gray;
+            this.butOK.Font = new System.Drawing.Font("Franklin Gothic Medium", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butOK.ForeColor = System.Drawing.Color.Black;
+            this.butOK.Location = new System.Drawing.Point(586, 432);
+            this.butOK.Name = "butOK";
+            this.butOK.Size = new System.Drawing.Size(154, 58);
+            this.butOK.TabIndex = 12;
+            this.butOK.Text = "OK";
+            this.butOK.UseVisualStyleBackColor = false;
+            this.butOK.Click += new System.EventHandler(this.button4_Click);
             // 
             // panel1
             // 
@@ -111,34 +111,34 @@
             this.panel1.Size = new System.Drawing.Size(80, 77);
             this.panel1.TabIndex = 13;
             // 
-            // radioButton1
+            // radioEntrance
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
-            this.radioButton1.Font = new System.Drawing.Font("Franklin Gothic Medium", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.ForeColor = System.Drawing.Color.White;
-            this.radioButton1.Location = new System.Drawing.Point(668, 204);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(147, 41);
-            this.radioButton1.TabIndex = 14;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Entrance";
-            this.radioButton1.UseVisualStyleBackColor = false;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.radioEntrance.AutoSize = true;
+            this.radioEntrance.BackColor = System.Drawing.Color.Transparent;
+            this.radioEntrance.Font = new System.Drawing.Font("Franklin Gothic Medium", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioEntrance.ForeColor = System.Drawing.Color.White;
+            this.radioEntrance.Location = new System.Drawing.Point(668, 204);
+            this.radioEntrance.Name = "radioEntrance";
+            this.radioEntrance.Size = new System.Drawing.Size(147, 41);
+            this.radioEntrance.TabIndex = 14;
+            this.radioEntrance.TabStop = true;
+            this.radioEntrance.Text = "Entrance";
+            this.radioEntrance.UseVisualStyleBackColor = false;
+            this.radioEntrance.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
-            // radioButton2
+            // radioExit
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
-            this.radioButton2.Font = new System.Drawing.Font("Franklin Gothic Medium", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.ForeColor = System.Drawing.Color.White;
-            this.radioButton2.Location = new System.Drawing.Point(844, 204);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(80, 41);
-            this.radioButton2.TabIndex = 15;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Exit";
-            this.radioButton2.UseVisualStyleBackColor = false;
+            this.radioExit.AutoSize = true;
+            this.radioExit.BackColor = System.Drawing.Color.Transparent;
+            this.radioExit.Font = new System.Drawing.Font("Franklin Gothic Medium", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioExit.ForeColor = System.Drawing.Color.White;
+            this.radioExit.Location = new System.Drawing.Point(844, 204);
+            this.radioExit.Name = "radioExit";
+            this.radioExit.Size = new System.Drawing.Size(80, 41);
+            this.radioExit.TabIndex = 15;
+            this.radioExit.TabStop = true;
+            this.radioExit.Text = "Exit";
+            this.radioExit.UseVisualStyleBackColor = false;
             // 
             // Form3
             // 
@@ -147,12 +147,12 @@
             this.BackgroundImage = global::Expressway_Admin_loin.Properties.Resources.Untitled_design_blue_4;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioExit);
+            this.Controls.Add(this.radioEntrance);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.butOK);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPoliceID);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
@@ -166,12 +166,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPoliceID;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button butOK;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioEntrance;
+        private System.Windows.Forms.RadioButton radioExit;
     }
 }
