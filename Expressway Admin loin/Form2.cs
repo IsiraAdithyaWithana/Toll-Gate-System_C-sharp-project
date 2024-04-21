b2946034c12d1ca2a6c211ba1dd8b046583a1708
SanudaDe <Sanudadesilva2003@gmail.com>
2024-04-21 22:04:10 +0530

    hu

--- Expressway Admin loin/Form2.cs
+++ Expressway Admin loin/Form2.cs
@@ -45,7 +45,7 @@ namespace Expressway_Admin_loin
         {
             try
             {
-                using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\balas\OneDrive\Desktop\Expressway Project C#\Expressway Admin loin\Database1.mdf"";Integrated Security=True"))
+                using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\ASUS\source\Repos\Toll-Gate-System_C-sharp-project\Expressway Admin loin\Database1.mdf"";Integrated Security=True"))
                 {
                     con.Open();
 
