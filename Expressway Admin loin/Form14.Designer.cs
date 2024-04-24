4143262dacf409c6590ab52f3a0c38dc3a8af12d
MandinuBala <balasooriyamandinu@gmail.com>
2024-04-25 03:03:50 +0530

    form 14 code created

--- Expressway Admin loin/Form14.Designer.cs
+++ Expressway Admin loin/Form14.Designer.cs
@@ -29,16 +29,18 @@
         private void InitializeComponent()
         {
             this.txtCardDetails = new System.Windows.Forms.Label();
-            this.txtname = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCNumber = new System.Windows.Forms.Label();
+            this.lblCVC = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtCNumber = new System.Windows.Forms.TextBox();
+            this.txtCVC = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtExpire = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtCardDetails
@@ -54,104 +56,107 @@
             this.txtCardDetails.TabIndex = 0;
             this.txtCardDetails.Text = "Card Details";
             // 
-            // txtname
+            // lblName
             // 
-            this.txtname.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtname.AutoSize = true;
-            this.txtname.BackColor = System.Drawing.Color.Transparent;
-            this.txtname.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtname.ForeColor = System.Drawing.Color.White;
-            this.txtname.Location = new System.Drawing.Point(218, 169);
-            this.txtname.Name = "txtname";
-            this.txtname.Size = new System.Drawing.Size(75, 30);
-            this.txtname.TabIndex = 1;
-            this.txtname.Text = "Name";
+            this.lblName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(218, 169);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(75, 30);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
             // 
-            // label3
+            // lblCNumber
             // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(218, 236);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(150, 30);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Card Number";
+            this.lblCNumber.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblCNumber.AutoSize = true;
+            this.lblCNumber.BackColor = System.Drawing.Color.Transparent;
+            this.lblCNumber.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCNumber.ForeColor = System.Drawing.Color.White;
+            this.lblCNumber.Location = new System.Drawing.Point(218, 236);
+            this.lblCNumber.Name = "lblCNumber";
+            this.lblCNumber.Size = new System.Drawing.Size(150, 30);
+            this.lblCNumber.TabIndex = 2;
+            this.lblCNumber.Text = "Card Number";
             // 
-            // label4
+            // lblCVC
             // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(218, 316);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 30);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "CVC";
+            this.lblCVC.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblCVC.AutoSize = true;
+            this.lblCVC.BackColor = System.Drawing.Color.Transparent;
+            this.lblCVC.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCVC.ForeColor = System.Drawing.Color.White;
+            this.lblCVC.Location = new System.Drawing.Point(218, 316);
+            this.lblCVC.Name = "lblCVC";
+            this.lblCVC.Size = new System.Drawing.Size(54, 30);
+            this.lblCVC.TabIndex = 3;
+            this.lblCVC.Text = "CVC";
             // 
             // button1
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button1.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(546, 387);
+            this.button1.Location = new System.Drawing.Point(553, 492);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(245, 35);
             this.button1.TabIndex = 4;
             this.button1.Text = "Proceed to pay";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button2.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(769, 481);
+            this.button2.Location = new System.Drawing.Point(835, 582);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(117, 37);
             this.button2.TabIndex = 5;
             this.button2.Text = "Clear";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button3.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(912, 481);
+            this.button3.Location = new System.Drawing.Point(987, 582);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(117, 37);
             this.button3.TabIndex = 6;
             this.button3.Text = "Done";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // textBox1
+            // txtName
             // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(426, 164);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(300, 35);
-            this.textBox1.TabIndex = 7;
+            this.txtName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(426, 164);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(300, 35);
+            this.txtName.TabIndex = 7;
             // 
-            // textBox2
+            // txtCNumber
             // 
-            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(426, 236);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(300, 35);
-            this.textBox2.TabIndex = 8;
+            this.txtCNumber.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtCNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCNumber.Location = new System.Drawing.Point(426, 236);
+            this.txtCNumber.Name = "txtCNumber";
+            this.txtCNumber.Size = new System.Drawing.Size(300, 35);
+            this.txtCNumber.TabIndex = 8;
             // 
-            // textBox3
+            // txtCVC
             // 
-            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(426, 316);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(200, 35);
-            this.textBox3.TabIndex = 9;
+            this.txtCVC.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtCVC.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCVC.Location = new System.Drawing.Point(426, 316);
+            this.txtCVC.Name = "txtCVC";
+            this.txtCVC.Size = new System.Drawing.Size(200, 35);
+            this.txtCVC.TabIndex = 9;
             // 
             // panel1
             // 
@@ -164,6 +169,32 @@
             this.panel1.Size = new System.Drawing.Size(80, 77);
             this.panel1.TabIndex = 10;
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Franklin Gothic Medium", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.CustomFormat = "MM/yyyy";
+            this.dateTimePicker1.Font = new System.Drawing.Font("Franklin Gothic Medium", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(426, 388);
+            this.dateTimePicker1.MaxDate = new System.DateTime(9998, 12, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 31);
+            this.dateTimePicker1.TabIndex = 11;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // txtExpire
+            // 
+            this.txtExpire.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtExpire.AutoSize = true;
+            this.txtExpire.BackColor = System.Drawing.Color.Transparent;
+            this.txtExpire.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExpire.ForeColor = System.Drawing.Color.White;
+            this.txtExpire.Location = new System.Drawing.Point(218, 389);
+            this.txtExpire.Name = "txtExpire";
+            this.txtExpire.Size = new System.Drawing.Size(133, 30);
+            this.txtExpire.TabIndex = 12;
+            this.txtExpire.Text = "Expire Date";
+            // 
             // Form14
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -172,19 +203,22 @@
             this.BackgroundImage = global::Expressway_Admin_loin.Properties.Resources.Untitled_design_blue_4;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.txtExpire);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtCVC);
+            this.Controls.Add(this.txtCNumber);
+            this.Controls.Add(this.txtName);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.lblCVC);
+            this.Controls.Add(this.lblCNumber);
+            this.Controls.Add(this.lblName);
             this.Controls.Add(this.txtCardDetails);
             this.Name = "Form14";
             this.Text = "Form14";
+            this.Load += new System.EventHandler(this.Form14_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -193,15 +227,17 @@
         #endregion
 
         private System.Windows.Forms.Label txtCardDetails;
-        private System.Windows.Forms.Label txtname;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCNumber;
+        private System.Windows.Forms.Label lblCVC;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCNumber;
+        private System.Windows.Forms.TextBox txtCVC;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label txtExpire;
     }
 }
--- Expressway Admin loin/Form14.cs
+++ Expressway Admin loin/Form14.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,42 @@ namespace Expressway_Admin_loin
         public Form14()
         {
             InitializeComponent();
+        }
+
+        private void Form14_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string cardnumber = txtCNumber.Text;
+            string cvc = txtCVC.Text;
+            string expiredate = dateTimePicker1.Text;
+
+            string message = $"Name: {name}\nCard Number: {cardnumber}\nCVV: {cvc}\nExpiration Date: {expiredate}";
+            MessageBox.Show(message, "Details Enterd");
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            txtCNumber.Text = "";
+            txtCVC.Text = "";
+            dateTimePicker1.Value = DateTime.Now; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ticket is printed");
+
+            ClearDetails();
         }
     }
 }
