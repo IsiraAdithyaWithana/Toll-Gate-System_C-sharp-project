f32afba16f4cf18075e8d1e118733ad28d848ca5
Isira Adithya Withana <issaadithyawithana@gmail.com>
2024-04-26 13:35:32 +0530

    form 11 hdnoo

--- Expressway Admin loin/Form11.cs
+++ Expressway Admin loin/Form11.cs
@@ -22,7 +22,40 @@ namespace Expressway_Admin_loin
 
         private void Form11_Load(object sender, EventArgs e)
         {
+            Dictionary<int, string> checkboxNameMap = new Dictionary<int, string>
+            {
+                { 0, "checkLeftSideOvertake" },
+                { 1, "checkSpeed" },
+                { 4, "checkDrunkDriving" },
+                { 5, "checkMobilePhones" },
+                { 6, "checkInvalidLicense" },
+                { 7, "checkNoise" },
+                { 10, "checkSpecificClassVehicle" }
+            };
+            if (!string.IsNullOrEmpty(Violations))
+            {
+                string[] violationValues = Violations.Split(' ');
 
+                foreach (string violation in violationValues)
+                {
+                    if (int.TryParse(violation, out int violationNumber))
+                    {
+                        if (checkboxNameMap.ContainsKey(violationNumber))
+                        {
+                            string checkboxName = checkboxNameMap[violationNumber];
+
+                            Control[] foundControls = Controls.Find(checkboxName, true);
+
+                            if (foundControls.Length > 0 && foundControls[0] is CheckBox)
+                            {
+                                CheckBox checkBox = (CheckBox)foundControls[0];
+
+                                checkBox.Checked = true;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
