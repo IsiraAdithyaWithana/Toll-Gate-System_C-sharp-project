5075c24be8d798b289aca5f80a7b7c2970a6577a
Isira Adithya Withana <issaadithyawithana@gmail.com>
2024-04-30 16:29:26 +0530

    form 14 hdnooo

--- Expressway Admin loin/Form14.cs
+++ Expressway Admin loin/Form14.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,10 +15,23 @@ namespace Expressway_Admin_loin
     public partial class Form14 : Form
       
     {
+        private string DriversLicense;
         private int userId;
-        public Form14()
+        string Violations;
+        string EorE;
+        string VehicleNumber;
+        string PaymentMethod;
+        int TotalFine;
+        string PaymentStatus;
+        public Form14(int UserId, string violations, string eorE, string vehicleNumber, string driverLicense, int totalFine)
         {
             InitializeComponent();
+            userId = UserId;
+            Violations = violations;
+            VehicleNumber = vehicleNumber;
+            EorE = eorE;
+            DriversLicense = driverLicense;
+            TotalFine = totalFine;
         }
 
         private void Form14_Load(object sender, EventArgs e)
@@ -77,6 +91,33 @@ namespace Expressway_Admin_loin
             Form9 form9 = new Form9(userId);
             form9.Show();
             this.Hide();
+        }
+
+        void SqlInjectionYes()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\isira\Desktop\Expressway project C#\Expressway Admin loin\Database1.mdf"";Integrated Security=True"))
+                {
+                    PaymentStatus = "Payed By Card";
+                    con.Open();
+                    string query = "INSERT INTO Payment ([userId],license_number,[fine_cost],payment_status) VALUES (@userId,@DriversLicense,@TotalFine,@PaymentStatus);";
+
+                    using (SqlCommand command = new SqlCommand(query, con))
+                    {
+                        command.Parameters.AddWithValue("@userId", userId);
+                        command.Parameters.AddWithValue("@DriversLicense", DriversLicense);
+                        command.Parameters.AddWithValue("@TotalFine", TotalFine);
+                        command.Parameters.AddWithValue("@PaymentStatus", PaymentStatus);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Error: " + Ex.Message);
+            }
         }
     }
 }
