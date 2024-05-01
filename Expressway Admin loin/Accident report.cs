535cde1b4eb15560ae3fda6f75073d6d8ae92f9e
Isira Adithya Withana <issaadithyawithana@gmail.com>
2024-05-02 05:24:00 +0530

    accident report haduwa

--- Expressway Admin loin/Accident report.cs
+++ Expressway Admin loin/Accident report.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Expressway_Admin_loin
 {
@@ -20,28 +21,67 @@ namespace Expressway_Admin_loin
 
         private void Accident_report_Load(object sender, EventArgs e)
         {
-            
+            chart1.ChartAreas[0].AxisX.Title = "Month";
+            chart1.ChartAreas[0].AxisY.Title = "Accident Count";
 
             try
             {
-                using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\balas\OneDrive\Desktop\Expressway Project C#\Expressway Admin loin\Database1.mdf"";Integrated Security=True"))
-                {
-                    con.Open();
-
-                    string query = "SELECT accident (user ,[nearest_point],[vehicle_type],date_time) FROM (@User, @NerestPoint, @VehicleType, @DateTime)";
-                    DataSet dataSet = new DataSet();
-
-                    adapter.Fill(dataSet, "accident");
-
-                    chart1.DataSource = dataSet.Tables["accident"];
-                    chart1.Series[""];
-                }
+                Dictionary<string, int> accidentCounts = GetAccidentCounts();
+                AddSeriesToChart();
+                BindDataToChart(accidentCounts);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
             }
         }
 
         private void chart1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private Dictionary<string, int> GetAccidentCounts()
+        {
+            Dictionary<string, int> accidentCounts = new Dictionary<string, int>();
+
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\isira\Desktop\Expressway project C#\Expressway Admin loin\Database1.mdf"";Integrated Security=True"))
+            {
+                con.Open();
+                string query = "SELECT date_time FROM accident";
+
+                SqlCommand command = new SqlCommand(query, con);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    DateTime dateTime = Convert.ToDateTime(reader["date_time"]);
+                    string monthYear = dateTime.ToString("MMM-yyyy");
+
+                    if (!accidentCounts.ContainsKey(monthYear))
+                    {
+                        accidentCounts[monthYear] = 0;
+                    }
+                    accidentCounts[monthYear]++;
+                }
+            }
+
+            return accidentCounts;
+        }
+
+        private void BindDataToChart(Dictionary<string, int> accidentCounts)
+        {
+            foreach (var entry in accidentCounts)
+            {
+                chart1.Series["AccidentCount"].Points.AddXY(entry.Key, entry.Value);
+            }
+        }
+
+        private void AddSeriesToChart()
+        {
+            Series series = new Series("AccidentCount");
+            series.ChartType = SeriesChartType.Column; 
+            chart1.Series.Add(series);
         }
     }
 }
