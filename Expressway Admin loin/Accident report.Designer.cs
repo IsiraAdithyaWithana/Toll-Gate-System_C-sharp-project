d8eb18f68dd020fb771b30881aedb935f2f9981a
Isira Adithya Withana <issaadithyawithana@gmail.com>
2024-05-02 05:33:02 +0530

    accident report haduwaa

--- Expressway Admin loin/Accident report.Designer.cs
+++ Expressway Admin loin/Accident report.Designer.cs
@@ -39,10 +39,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(415, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.Size = new System.Drawing.Size(179, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Accident report ";
             // 
