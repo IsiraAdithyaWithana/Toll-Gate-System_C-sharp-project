f5054a755c188f6c37b13aaa4d56096e5ba55e19
MandinuBala <balasooriyamandinu@gmail.com>
2024-05-02 17:05:31 +0530

    fine report

--- Expressway Admin loin/Fine report.Designer.cs
+++ Expressway Admin loin/Fine report.Designer.cs
@@ -28,27 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(185, 93);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            series2.Legend = "Legend1";
+            series2.Name = "Fnes ";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(611, 364);
+            this.chart1.TabIndex = 2;
+            this.chart1.TabStop = false;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(341, 213);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(388, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Fine report";
+            this.label1.Size = new System.Drawing.Size(134, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fine report ";
             // 
             // Fine_report
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
             this.Cursor = System.Windows.Forms.Cursors.No;
             this.Name = "Fine_report";
             this.Size = new System.Drawing.Size(975, 477);
+            this.Load += new System.EventHandler(this.Fine_report_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +84,7 @@
 
         #endregion
 
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Label label1;
     }
 }
--- Expressway Admin loin/Fine report.cs
+++ Expressway Admin loin/Fine report.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Expressway_Admin_loin
 {
@@ -16,5 +18,71 @@ namespace Expressway_Admin_loin
         {
             InitializeComponent();
         }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Fine_report_Load(object sender, EventArgs e)
+        {
+            chart1.ChartAreas[0].AxisX.Title = "Month";
+            chart1.ChartAreas[0].AxisY.Title = "Fine Count ";
+
+            try
+            {
+                Dictionary<string, int> FineCounts = GetFineCounts();
+                AddSeriesToChart();
+                BindDataToChart(FineCounts);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private Dictionary<string, int> GetFineCounts()
+        {
+            Dictionary<string, int> FineCounts = new Dictionary<string, int>();
+
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\balas\OneDrive\Desktop\Expressway Project C#\Expressway Admin loin\Database1.mdf"";Integrated Security=True"))
+            {
+                con.Open();
+                string query = "SELECT date_time FROM accident";
+
+                SqlCommand command = new SqlCommand(query, con);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    DateTime dateTime = Convert.ToDateTime(reader["date_time"]);
+                    string monthYear = dateTime.ToString("MMM-yyyy");
+
+                    if (!FineCounts.ContainsKey(monthYear))
+                    {
+                        FineCounts[monthYear] = 0;
+                    }
+                    FineCounts[monthYear]++;
+                }
+            }
+
+            return FineCounts;
+        }
+
+        private void BindDataToChart(Dictionary<string, int> accidentCounts)
+        {
+            foreach (var entry in accidentCounts)
+            {
+                chart1.Series["AccidentCount"].Points.AddXY(entry.Key, entry.Value);
+            }
+        }
+
+        private void AddSeriesToChart()
+        {
+            Series series = new Series("AccidentCount");
+            series.ChartType = SeriesChartType.Column;
+            chart1.Series.Add(series);
+        }
     }
+
 }
